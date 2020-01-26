4ef3d0d35bdeeb0a26477986ada8053bd89b1a33
turtleman9000 <49375077+turtleman9000@users.noreply.github.com>
2020-01-26 12:20:07 -0800

    add files via upload
    yeet

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = 4;
+let b = 3;
+let c = Math.sqrt (Math.pow(a,2) + Math.pow(b,2));
+document.getElementById('a').innerHTML = "a = " + a;
+document.getElementById('b').innerHTML = "b = " + b;
+document.getElementById('c').innerHTML = "result = " + c;
